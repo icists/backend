49db809a286ff0372263d999d8d9023132064595
Bill Jang <malloc099@gmail.com>
2019-02-17 14:38:28 +0900

    Mail Front Building

--- mail/views.py
+++ mail/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'mail/index.html', context={})
 
 def _input_email_conf(request):
     # Receive Email Configuration
